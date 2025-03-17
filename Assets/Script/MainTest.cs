c6bd60367905674cedbfa222ead2310e96083393
suyan05 <cadhppr588@gmail.com>
2025-03-17 15:48:38 +0900

    [Fead] 소스코드 커밋

--- Assets/Script/MainTest.cs
+++ Assets/Script/MainTest.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MainTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 1;
+
+    public void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
+
+
 }
